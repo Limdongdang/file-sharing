6f4254bd07225cc4db472ba6165bdb44b3e51fba
LimDong <50188317+Limdongdang@users.noreply.github.com>
2025-02-18 01:35:52 +0900

    fix(front) : vite env config

--- apps/frontend/src/services/config.js
+++ apps/frontend/src/services/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000';
 
 export const instance = axios.create({
   withCredentials: true,
