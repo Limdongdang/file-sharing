d5f89b0a34d156e494a120fd0661ba7aed0dc8d5
LimDong <50188317+Limdongdang@users.noreply.github.com>
2025-02-18 02:39:13 +0900

    fix(back) : cors.js

--- apps/backend/src/config/cors.js
+++ apps/backend/src/config/cors.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const corsOptions = {
     origin: process.env.CORS_ORIGIN || 'http://localhost:8080',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
